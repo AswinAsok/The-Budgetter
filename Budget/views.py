dc945e28cfc02091f135c4e391806665a9aba591
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-20 19:00:47 +0530

    Data from Db loaded

--- Budget/views.py
+++ Budget/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Budget
 
 # Create your views here.
+
+def home(request):
+    Budgets = Budget.objects.all()
+    context = {}
+    context['Budgets'] = Budgets
+    print(Budgets)
+    return render(request, 'home.html', context)
