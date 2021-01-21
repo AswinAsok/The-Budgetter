6f38f47377a43d6b9b5e2d39a5cb4ddd2ad537d9
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-21 19:10:32 +0530

    Navbar customizations done

--- Budgetter/urls.py
+++ Budgetter/urls.py
@@ -5,10 +5,10 @@ from Budget.views import history,create,home,signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', signup, name='signup'),
+    path('account/signup/', signup, name='signup'),
     path('history/', history, name='history'),
     path('create/', create, name='create'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('account/', include('django.contrib.auth.urls')),
     path('', home, name='home')
    
 ]
