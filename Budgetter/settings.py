cce26fc3eb3507f3d9d0694c56e48ffef5f99225
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-23 19:21:42 +0530

    Design Changes Made

--- Budgetter/settings.py
+++ Budgetter/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
