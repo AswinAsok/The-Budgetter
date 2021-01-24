0262b7836b7e943c8da886ed970a024be2235b08
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-24 08:02:32 +0530

    Deployment V3

--- Budgetter/settings.py
+++ Budgetter/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
+import dj_database_url
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
